5bb91a5b65cf279cdbbeae45c7c1b78c0e9871d2
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 10:35:06 -0800

    Create registrations table

--- db/migrate/20221111183203_create_registrations.rb
+++ db/migrate/20221111183203_create_registrations.rb
@@ -1,4 +1,9 @@
 class CreateRegistrations < ActiveRecord::Migration[6.1]
   def change
+    create_table :registrations do |t|
+      t.integer :program_id
+      t.integer :student_id
+      t.timestamps
+    end
   end
 end
