8780114f8d3405c0f140d6989abeb62be1bf2f90
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 10:40:55 -0800

    Delete migrations

--- db/migrate/20221111183238_create_programs.rb
+++
@@ -1,9 +0,0 @@
-class CreatePrograms < ActiveRecord::Migration[6.1]
-  def change
-    create_table :programs do |t|
-      t.string :title
-      t.string :teacher
-      t.timestamps
-    end
-  end
-end
