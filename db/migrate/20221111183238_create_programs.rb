6dcc3719e090c1201a28a7fdd65f578da3648dca
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 10:36:03 -0800

    Create programs table

--- db/migrate/20221111183238_create_programs.rb
+++ db/migrate/20221111183238_create_programs.rb
@@ -1,4 +1,9 @@
 class CreatePrograms < ActiveRecord::Migration[6.1]
   def change
+    create_table :programs do |t|
+      t.string :title
+      t.string :teacher
+      t.timestamps
+    end
   end
 end
