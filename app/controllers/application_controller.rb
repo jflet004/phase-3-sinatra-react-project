f3e6175ccda41bd8e8dfbb8e339f4728b17322ae
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 11:23:25 -0800

    Get all program data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,26 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
 
+  #Student routes
   get "/students" do
     students = Student.all
-    students.to_json
+    students.to_json(include: :programs)
   end
   
   get "/students/:id" do
     student = Student.find(params[:id])
-    student.to_json
+    student.to_json(include: :programs)
+  end
+  
+  #Program routes
+  get "/programs" do
+    programs = Program.all
+    programs.to_json(include: :students)
+  end
+
+  get "/programs/:id" do
+    program = Program.find(params[:id])
+    program.to_json(include: :students)
   end
 
 
