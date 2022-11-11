43d26c626e4abd79517000add88f2187228ed057
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 10:49:07 -0800

    Create student seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+#Student data
+s1 = Student.create(first_name: "Jose", last_name: "Fletes", age: 33, active: false)
+s2 = Student.create(first_name: "Cassandra", last_name: "Fletes", age: 30, active: false)
+s3 = Student.create(first_name: "Tahnee", last_name: "Freda", age: 33, active: true)
+s4 = Student.create(first_name: "Luis", last_name: "Osorio", age: 24, active: true)
+s5 = Student.create(first_name: "Nadia", last_name: "Aponte", age: 23, active: true)
 
 puts "✅ Done seeding!"
