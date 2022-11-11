f90b133aa56a2b0b6dc3181de9351fdc97428c4d
Jose Fletes <joseangelfletes@gmail.com>
2022-11-11 11:30:11 -0800

    Post and Delete controller for students

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,17 @@ class ApplicationController < Sinatra::Base
     student = Student.find(params[:id])
     student.to_json(include: :programs)
   end
+
+  post "/students" do
+    student = Student.create(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], active: params[:active])
+    student.to_json
+  end
+  
+  delete "/students/:id" do
+    student = Student.find(params[:id])
+    student.destroy
+    student.to_json
+  end
   
   #Program routes
   get "/programs" do
